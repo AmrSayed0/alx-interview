1c72d29a33ebffee989fd29d54475be5ef7461b8
AmrSayed0 <amracademycode@gmail.com>
2023-09-29 01:34:42 +0300

    Fix bug: Create a module for printing Pascal's Triangle

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """
-This script is the main module for generating and printing Pascal's Triangle.
+0-main
 """
 pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
 
 def print_triangle(triangle):
     """
-    Print the Pascal's Triangle.
+    Print the triangle
     """
-    # Iterate through each row in the triangle and print it
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
 
+
 if __name__ == "__main__":
-    # Generate and print Pascal's Triangle with 5 rows
     print_triangle(pascal_triangle(5))
