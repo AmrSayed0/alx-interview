4ec6958fd333554cd584e5c4f499786a76742b50
AmrSayed0 <amracademycode@gmail.com>
2023-10-05 18:36:59 +0300

    Fix logic error in canUnlockAll function

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,15 +3,7 @@
 
 
 def canUnlockAll(boxes):
-    """
-    Determines if all the boxes can be opened
-
-    Args:
-        boxes (list): A list of lists containing keys to other boxes.
-    
-    Returns:
-        bool: True if all boxes can be opened, else False.
-    """
+    """Determines if all the boxes can be opened"""
     if (type(boxes) is not list or len(boxes) == 0):
         return False
     for k in range(1, len(boxes) - 1):
